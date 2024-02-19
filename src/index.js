7fe9132cce6ecc2d6a0ef6d38df1879ea2b7a7f1
Siv0k <artvlad2003@gmail.com>
2024-02-19 13:06:25 +0300

    check for empty input when creating a class instance

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ const inputButton = document.getElementById('inputButton');
 
 inputButton.addEventListener('click', () => {
 	const inputData = input.value;
+
+	if (inputData.trim() === '') {
+		alert('Введите данные!');
+		return;
+	}
+
 	new Histogram(inputData);
 	input.value = '';
 });
